d9b69294a7ac18493e077c7602ec7ae679cd7275
Maurice Snoeren <mac.snoeren@avans.nl>
2024-12-01 23:31:19 +0100

    Small changes to fix the logics of the web page

--- Firmware/sm-esp8266/sm-esp8266.ino
+++ Firmware/sm-esp8266/sm-esp8266.ino
@@ -980,13 +980,20 @@ void handleRoot() {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>SmartMeter DIY</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.6/dist/chart.umd.min.js" integrity="sha384-Sse/HDqcypGpyTDpvZOJNnG0TT3feGQUkF9H+mnRvic+LjR+K1NhTt8f51KIQ3v3" crossorigin="anonymous"></script>
  </head>
  <body>
-  Hello world
   <script>
 $(document).ready(function(){
-  $("<p>Appended</p>").appendTo("body");
+ $.ajax({
+    url: "https://raw.githubusercontent.com/AvansETI/SmartMeter_DIY/refs/heads/feature/webserver/Firmware/sm-esp8266/web/body.html",
+    success: function (data) { $('body').append(data); },
+    dataType: 'html'
+ });
 });
   </script>
  </body>
