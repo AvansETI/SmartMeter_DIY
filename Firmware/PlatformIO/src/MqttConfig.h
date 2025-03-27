3e1690f7335e6784c70c7636f09cc629fd965027
macsnoeren <macsnoeren@gmail.com>
2025-03-27 23:12:08 +0100

    Removed PlatformIO source, while this is not maintained anymore and changed the text of README.md

--- Firmware/PlatformIO/src/MqttConfig.h
+++
@@ -1,6 +0,0 @@
-#define MQTT_USERNAME "smartmeter"
-#define MQTT_PASSWORD "se_smartmeter"
-#define MQTT_REMOTE_HOST "sendlab.nl"
-#define MQTT_REMOTE_PORT "11883"
-#define MQTT_TOPIC "smartmeter/raw"
-#define MQTT_MSGBUF_SIZE 2048
