73eaef0f6fca8a4186b59a0cb82e525240b82057
Arno Broeders <ahc.broeders@avans.nl>
2025-07-25 20:44:31 +0200

    Changed mqtt domainname to use the right hostname.

--- Firmware/sm-esp8266/MqttSendlab.h
+++ Firmware/sm-esp8266/MqttSendlab.h
@@ -22,7 +22,7 @@
 */
 #define MQTT_USERNAME "smartmeter"
 #define MQTT_PASSWORD "se_smartmeter"
-#define MQTT_REMOTE_HOST "sendlab.nl"
+#define MQTT_REMOTE_HOST "mqtt.sendlab.nl"
 #define MQTT_REMOTE_PORT "11883"
 #define MQTT_TOPIC "smartmeter/raw"
 #define MQTT_MSGBUF_SIZE 2048
